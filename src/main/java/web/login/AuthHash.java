e3c2cf864f887f3e4936539e9f1d0080cf7286ac
zeynab-mammadova <zeynebb2022@gmail.com>
2020-04-17 02:20:41 +0400

    commit

--- src/main/java/web/login/AuthHash.java
+++ src/main/java/web/login/AuthHash.java
@@ -5,15 +5,15 @@ import java.util.Map;
 
 public class AuthHash implements Auth {
 
-  private final Map<String, String> db = new HashMap<String, String>(){{
-    put("user", "123");
-    put("admin", "234");
-    put("manager", "345");
+  private final Map<String, String> database = new HashMap<String, String>(){{
+    put("zeynab", "12345");
+    put("alexey", "54321");
+    put("mom", "1212");
   }};
 
   @Override
   public boolean check(String name, String password) {
-    return db.containsKey(name) && db.get(name).equals(password);
+    return database.containsKey(name) && database.get(name).equals(password);
   }
 }
 
